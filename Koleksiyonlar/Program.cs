cfd252798c57a991efd550b3ed6376bbc7e9bc12
Berna Delikanlı <berna.delikanli@YONDER.LOCAL>
2021-01-21 22:45:13 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String[] isimler = new string[] { "Ali", "Ayşe", "Can" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+
+            List<string> isimler2 = new List<string> { "Ali", "Ayşe", "Can" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("Berna");
+            isimler2.Add("Ayça");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[4]);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//manager Operasyon dosyası
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product) //encapsulation
+        {
+            Console.WriteLine(product.ProductName+" Eklendi");
+        }
+        public void Update(Product product)     //void=
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+
+        
+    }
+}
+
+//int str double değer tip değişmez     
+//class diziler  referans tip değişir
